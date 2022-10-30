892804eab82f214d872d2014a7f02f068e7021f9
NesrinMehany <nesreen.safwat87@gmail.com>
2022-10-29 21:39:20 -0400

    collection frame work

--- src/com/syntax/Class18/Animal.java
+++ src/com/syntax/Class18/Animal.java
@@ -3,10 +3,13 @@ package com.syntax.Class18;
 
     public class Animal {
         protected   String name;
-        String breed;
+       String breed;
         int age;
-        double weight;
+         double weight;
 
+       public String  getBreed(){
+            return breed;
+        }
         void printInfo(){
             String name="now this will be printed";
             System.out.println("name "+this.name+" Breed "+breed+" age "+age+" Weight "+weight);
